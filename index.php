84ce92f03a0f5fcd126ad0fa6398d48797761f40
Kazae67 <Kazae.zaeka@gmail.com>
2023-06-10 03:30:07 +0200

    sauvegarde

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once 'app/controllers/Controller.php';
-require_once 'app/models/Model.php';
+require_once 'app/models/Connect.php';
 include 'app/views/templates/header.php';
 include 'app/views/templates/footer.php'; 
 
