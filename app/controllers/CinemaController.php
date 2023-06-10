8d039b8c2712344c196c2da8523dffccf591ce22
Kazae67 <Kazae.zaeka@gmail.com>
2023-06-10 15:46:19 +0200

    fetchall()

--- app/controllers/CinemaController.php
+++ app/controllers/CinemaController.php
@@ -14,38 +14,11 @@ class CinemaController {
         SELECT titre, annee_sortie
         FROM film
         ");
+        
+        // Récupérer les résultats de la requête
+        $films = $requete->fetchAll();
 
         require "view/listFilms.php";
     }
 }
-
 ?>
-
-<?php ob_start(); ?>
-
-<p class="uk-label uk-label-warning"> Il y a <?= $requete->rowCount() ?> films</p>
-
-<table class="uk-table uk-table-striped">
-    <thead>
-        <tr>
-            <th>TITRE</th>
-            <th>ANNEE SORTIE</th>
-        </tr>
-    </thead>
-    <tbody>
-        <?php
-            foreach($requete->fetchAll() as $film) {?>
-                <tr>
-                    <td><?= $film["titre"] ?></td>
-                    <td><?= $film["annee_sortie"] ?></td>
-                </tr>
-           <?php } ?>
-    </tbody>
-</table>
-
-<?php
-
-$titre = "Liste des films";
-$titre_secondaire = "Liste des films";
-$contenu = ob_get_clean();
-
--- index.php
+++ index.php
@@ -1,7 +1,22 @@
 <?php
 
-require_once 'app/controllers/Controller.php';
+require_once 'app/controllers/CinemaController.php';
 require_once 'app/models/Connect.php';
 include 'app/views/templates/header.php';
 include 'app/views/templates/footer.php'; 
 
+use Controller\CinemaController;
+
+spl_autoload_register(function ($class_name){
+    include $class_name . '.php';
+});
+
+$ctrlCinema = new CinemaController();
+
+if(isset($_GET["action"])){
+    switch ($_GET["action"]){
+
+        case "listFilms" : $ctrlCinema->listFilms(); break;
+        case "listActeurs" : $ctrlCinema->listActeurs(); break;
+    }
+}
