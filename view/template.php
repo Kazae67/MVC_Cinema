94e7c3dd0cd4abba20399266e1ea976e0ba50e64
Kazae67 <Kazae.zaeka@gmail.com>
2023-06-12 00:07:31 +0200

    déplacement style.css

--- view/template.php
+++ view/template.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://kit.fontawesome.com/a45e9c27c8.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="public/css/style.css">
     <title>Cinema</title>
 </head>
 
