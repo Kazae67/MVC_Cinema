7e2d1e85986a64735fcc496601ce0bde146ad6b7
Kazae67 <Kazae.zaeka@gmail.com>
2023-06-12 05:38:39 +0200

    ajout de condition code couleur étoile

--- view/detailsFilm.php
+++ view/detailsFilm.php
@@ -5,49 +5,57 @@ $film = $request_film->fetch();
 
 <a href="index.php?action=listFilms" class="button"><i class="fa-solid fa-arrow-left"></i>Retour</a>
 <div class="container">
-<div class="content">
-<div class="film-card-list">
-    <div class="film-card-infos">
-        <div class="film-card-detail">
-            <span><?= $film["titre_film"] ?></span>
-            <span>Realisateur : <a href="index.php?action=detailsRealisateur&id=<?= $film['id_realisateur'] ?>"><?= $film["rea_prenom"] . " " . $film["rea_nom"] ?></a></span>
-            <span>Genre : <?= ucfirst($film["genre_name"]) ?></span>
-            <span>Date de sortie : <?= $film["date_sortie"] ?></span>
-            <span>Durée du film :
-                <?php
-                $minutes = $film["duree"];
-                $duree = date('H:i', mktime(0, $minutes));
-                echo $duree;
-                ?>
-            </span>
-            <span>Note :
-                <?php
-                $times = $film["note"];
-                echo str_repeat("<i class='fa-solid fa-star'></i>", $times);
-                echo str_repeat("<i class='fa-regular fa-star'></i>", 5 - $times);
-                ?>
-            </span>
-            <span>Synopsis : <?= $film["synopsis"] ?></span>
+    <div class="content">
+        <div class="film-card-list">
+            <div class="film-card-infos">
+                <div class="film-card-detail">
+                    <span><?= $film["titre_film"] ?></span>
+                    <span>Realisateur : <a href="index.php?action=detailsRealisateur&id=<?= $film['id_realisateur'] ?>"><?= $film["rea_prenom"] . " " . $film["rea_nom"] ?></a></span>
+                    <span>Genre : <?= ucfirst($film["genre_name"]) ?></span>
+                    <span>Date de sortie : <?= $film["date_sortie"] ?></span>
+                    <span>Durée du film :
+                        <?php
+                        $minutes = $film["duree"];
+                        $duree = date('H:i', mktime(0, $minutes));
+                        echo $duree;
+                        ?>
+                    </span>
+                    <span>Note :
+                        <?php
+                        $note = $film["note"];
+                        if ($note >= 0 && $note <= 2) {
+                            echo str_repeat("<i class='fa-solid fa-star red-star'></i>", $note);
+                            echo str_repeat("<i class='fa-regular fa-star'></i>", 5 - $note);
+                        } elseif ($note >= 3 && $note <= 4) {
+                            echo str_repeat("<i class='fa-solid fa-star blue-star'></i>", $note);
+                            echo str_repeat("<i class='fa-regular fa-star'></i>", 5 - $note);
+                        } elseif ($note == 5) {
+                            echo str_repeat("<i class='fa-solid fa-star gold-star'></i>", $note);
+                        }
+                        ?>
+                    </span>
+                    <span>Synopsis : <?= $film["synopsis"] ?></span>
 
-            <?php
-            if ($request_casting->rowCount() > 0) {
-                ?>
-                <span>Il y a un total de <?= $request_casting->rowCount() ?> acteurs dans ce film :</span>
-                <?php
-                foreach ($request_casting->fetchAll() as $casting) {
-                    ?>
-                    <span> - <a href="index.php?action=detailsActeur&id=<?= $casting['id_acteur'] ?>"><?= $casting["prenom"] . " " . $casting["nom"] ?></a> dans le role de <?= $casting["role_name"] ?></span>
                     <?php
-                }
-            } else {
-                ?>
-                <span class="error">Aucun acteurs ajoutés à ce film.</span>
-                <?php
-            }
-            ?>
+                    if ($request_casting->rowCount() > 0) {
+                        ?>
+                        <span>Il y a un total de <?= $request_casting->rowCount() ?> acteurs dans ce film :</span>
+                        <?php
+                        foreach ($request_casting->fetchAll() as $casting) {
+                            ?>
+                            <span> - <a href="index.php?action=detailsActeur&id=<?= $casting['id_acteur'] ?>"><?= $casting["prenom"] . " " . $casting["nom"] ?></a> dans le rôle de <?= $casting["role_name"] ?></span>
+                            <?php
+                        }
+                    } else {
+                        ?>
+                        <span class="error">Aucun acteur ajouté à ce film.</span>
+                        <?php
+                    }
+                    ?>
 
-            <img class="image-film-xl" src="<?= $film["url_img"] ?>" alt="affiche <?= $film["titre_film"] ?>">
-            
+                    <img class="image-film-xl" src="<?= $film["url_img"] ?>" alt="affiche <?= $film["titre_film"] ?>">
+                </div>
+            </div>
         </div>
     </div>
 </div>
