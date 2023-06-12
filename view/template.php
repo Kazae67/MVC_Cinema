e934602355ed94a85a75c148e537423a549d8cb0
Kazae67 <Kazae.zaeka@gmail.com>
2023-06-12 05:55:10 +0200

    organisation des fichiers css

--- view/template.php
+++ view/template.php
@@ -6,7 +6,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://kit.fontawesome.com/a45e9c27c8.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="public/css/detailFilm.css">
+    <link rel="stylesheet" href="public/css/etoiles.css">
+    <link rel="stylesheet" href="public/css/film.css">
+    <link rel="stylesheet" href="public/css/navbar.css">
+    <link rel="stylesheet" href="public/css/standard.css">
     <link rel="stylesheet" href="public/css/style.css">
+
     <title>Cinema</title>
 </head>
 
@@ -16,10 +22,10 @@
             <h2 class="logo"><a href="index.php?action=listFilms">Cinema</a></h2>
             <ul class="menu">
                 <li><a href="index.php?action=listFilms">Films</a></li>
-                <li><a href="index.php?action=listGenres">Genre</a></li>
                 <li><a href="index.php?action=listActeurs">Acteurs</a></li>
                 <li><a href="index.php?action=listRoles">Rôles</a></li>
                 <li><a href="index.php?action=listRealisateurs">Réalisateurs</a></li>
+                <li><a href="index.php?action=listGenres">Genre</a></li>
                 <li><a href="index.php?action=listCastings">Castings</a></li>
             </ul>
         </nav>
