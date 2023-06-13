cc6e95d9aabf5f3ab7264b96de4827e1255a1103
Kazae67 <Kazae.zaeka@gmail.com>
2023-06-13 22:17:36 +0200

    path remplacé pour avoir accès directement à l'image + extension de la base de donné

--- view/acteur/listActeurs.php
+++ view/acteur/listActeurs.php
@@ -1,26 +1,31 @@
 <?php
 ob_start();
 ?>
-<?php $liste = "Les acteurs disponible".(($request->rowCount() > 1) ? "s" : ""); ?>
+<?php $liste = "Les acteurs disponibles" . (($request->rowCount() > 1) ? "s" : ""); ?>
 <p class="row-count-list">Un total de <?= $request->rowCount() ?> acteur<?= ($request->rowCount() > 1) ? "s" : "" ?> disponible<?= ($request->rowCount() > 1) ? "s" : "" ?></p>
 
 <div class="acteur-card-list">
-        <?php foreach ($request->fetchAll() as $acteur): ?>
-                <a href="index.php?action=infosActeur&id=<?= $acteur["id_acteur"] ?>">
-                <div class="acteur-card">
-                        <div class="acteur-card-infos">
-                             <span><?= ucfirst($acteur["nom"])." ".ucfirst($acteur["prenom"]) ?></span>
-                             <span><?= "Né(e) : ".$acteur["birthdate"] ?></span>
-                             <span><?= "Sexe : ".$acteur["sexe"] ?></span>
-                        </div>
-                        <img class="image-acteur" src="<?= $acteur["acteur_url_img"] ?>" alt="affiche de acteur">
+    <?php foreach ($request->fetchAll() as $acteur): ?>
+        <a href="index.php?action=infosActeur&id=<?= $acteur["id_acteur"] ?>">
+            <div class="acteur-card">
+                <div class="acteur-card-infos">
+                    <span><?= ucfirst($acteur["nom"]) . " " . ucfirst($acteur["prenom"]) ?></span>
+                    <span><?= "Né(e) : " . $acteur["birthdate"] ?></span>
+                    <span><?= "Sexe : " . $acteur["sexe"] ?></span>
                 </div>
-             </a>
-        <?php endforeach;?>
-        </div>
+                <!-- IMAGE -->
+                <?php 
+                $imagePath = "public/images/imgActeurs/";
+                $imageFilename = $acteur["acteur_url_img"];
+                $imageUrl = $imagePath . $imageFilename;
+                ?>
+                <img class="image-acteur" src="<?= $imageUrl ?>" alt="affiche de l'acteur <?= ucfirst($acteur["nom"]) . " " . ucfirst($acteur["prenom"]) ?>">
+            </div>
+        </a>
+    <?php endforeach;?>
+</div>
 
 <?php
-
 $content = ob_get_clean();
 require "view/template.php";
-
+?>
