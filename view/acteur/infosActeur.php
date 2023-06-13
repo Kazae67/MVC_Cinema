e45cfc26c4ad5c4d1f9990cb4d40a977fd614a93
Kazae67 <Kazae.zaeka@gmail.com>
2023-06-13 22:30:00 +0200

    changement du path pour accéder à ma bdd par son extension (exemple xxx.jpg)

--- view/acteur/infosActeur.php
+++ view/acteur/infosActeur.php
@@ -2,14 +2,14 @@
 ob_start();
 ?>
 <div class="film-acteur-card-list">
-	<div class="film-acteur-card-detail">
-		<?php if (isset($request_acteur_infos)) {
-			$acteur_infos = $request_acteur_infos->fetch(); ?>
+    <div class="film-acteur-card-detail">
+        <?php if (isset($request_acteur_infos)) {
+            $acteur_infos = $request_acteur_infos->fetch(); ?>
             <h2><?= $acteur_infos["prenom"] . " " . $acteur_infos["nom"] ?></h2>
-			
-			<p class="row-count-list">
-				Cet acteur a joué un total de : <?= $request_acteur_list_films->rowCount() ?> film<?= ($request_acteur_list_films->rowCount() > 1) ? "s" : "" ?>
-			</p>
+
+            <p class="row-count-list">
+                Cet acteur a joué un total de : <?= $request_acteur_list_films->rowCount() ?> film<?= ($request_acteur_list_films->rowCount() > 1) ? "s" : "" ?>
+            </p>
             <span>Né le : <?= $acteur_infos["birthdate"] ?></span>
 
             <span>
@@ -23,7 +23,13 @@ ob_start();
                     <div class="film-container">
                         <a href="index.php?action=infosFilm&id=<?= $acteur_film['id_film'] ?>">
                             <div class="film-acteur-card">
-                                <img class="image-acteur-film" src="<?= $acteur_film['path_img_film'] ?>" alt="affiche du film <?= $acteur_film['titre_film'] ?>">
+                                <!-- IMAGE -->
+                                <?php 
+                                $imagePath = "public/images/imgFilms/";
+                                $imageFilename = $acteur_film["path_img_film"];
+                                $imageUrl = $imagePath . $imageFilename;
+                                ?>
+                                <img class="image-acteur-film" src="<?= $imageUrl ?>" alt="affiche du film <?= $acteur_film['titre_film'] ?>">
                                 <div class="film-info">
                                     <span class="film-title"><?= $acteur_film['titre_film'] ?></span>
                                     <span class="film-role">Rôle : <?= $acteur_film['role_name'] ?></span>
