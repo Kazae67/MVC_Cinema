f622c3b73dc7bf1abb0acc8b4c0872ddfb18cadd
Kazae67 <Kazae.zaeka@gmail.com>
2023-06-15 00:00:00 +0200

    correction alignement RealisateurController.php

--- controller/RealisateursController.php
+++ controller/RealisateursController.php
@@ -10,37 +10,35 @@ class RealisateursController {
     {
         $pdo = Connect::connectToDb();
         $request = $pdo->query("
-        SELECT prenom, nom, sexe, birthdate, id_realisateur, path_img_realisateur
-        FROM realisateur
+            SELECT prenom, nom, sexe, birthdate, id_realisateur, path_img_realisateur
+            FROM realisateur
         ");
 
         require "view/realisateur/listRealisateurs.php";
     }
 
+    // Infos REALISATEUR
+    public function infosRealisateur($id_realisateur)
+    {
+        $pdo = Connect::connectToDb();
 
-// Infos REALISATEUR
-public function infosRealisateur($id_realisateur)
-{
-    $pdo = Connect::connectToDb();
+        $request_realisateur_infos = $pdo->prepare("
+            SELECT DISTINCT r.id_realisateur, r.prenom, r.nom, r.birthdate, r.path_img_realisateur, r.biographie
+            FROM realisateur r
+            WHERE r.id_realisateur = :id_realisateur
+        ");
 
-    $request_realisateur_infos = $pdo->prepare("
-        SELECT DISTINCT r.id_realisateur, r.prenom, r.nom, r.birthdate, r.path_img_realisateur, r.biographie
-        FROM realisateur r
-        WHERE r.id_realisateur = :id_realisateur
-    ");
+        $request_realisateur_infos->execute(["id_realisateur" => $id_realisateur]);
 
-    $request_realisateur_infos->execute(["id_realisateur" => $id_realisateur]);
+        $request_realisateur_list_films = $pdo->prepare("
+            SELECT f.titre_film, f.date_sortie, f.duree, f.path_img_film, id_film
+            FROM realisateur r
+            INNER JOIN film f ON f.realisateur_id = r.id_realisateur
+            WHERE r.id_realisateur = :id_realisateur
+        ");
 
-    $request_realisateur_list_films = $pdo->prepare("
-        SELECT f.titre_film, f.date_sortie, f.duree, f.path_img_film, id_film
-        FROM realisateur r
-        INNER JOIN film f ON f.realisateur_id = r.id_realisateur
-        WHERE r.id_realisateur = :id_realisateur
-    ");
+        $request_realisateur_list_films->execute(["id_realisateur" => $id_realisateur]);
 
-    $request_realisateur_list_films->execute(["id_realisateur" => $id_realisateur]);
-
-    require "view/realisateur/infosRealisateur.php";
-}
-
+        require "view/realisateur/infosRealisateur.php";
+    }
 }
