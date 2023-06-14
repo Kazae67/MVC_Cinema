a783f37fc4318ee9b206677e586af7e41d08808c
Kazae67 <Kazae.zaeka@gmail.com>
2023-06-14 23:47:36 +0200

    ajout des images, cards, css, ainsi que la possibilité de cliquer sur l'image pour accéder à infosRealisateur&id

--- view/realisateur/listRealisateurs.php
+++ view/realisateur/listRealisateurs.php
@@ -1,33 +1,29 @@
 <?php ob_start(); ?>
 
-<?php $liste = "Les réalisateurs disponibles" . (($request->rowCount() > 1) ? "s" : ""); ?>
+<?php
+$liste = "Les réalisateurs disponibles" . (($request->rowCount() > 1) ? "s" : "");
+?>
 <p class="row-count-list">Un total de <?= $request->rowCount() ?> réalisateur<?= ($request->rowCount() > 1) ? "s" : "" ?> disponible<?= ($request->rowCount() > 1) ? "s" : "" ?></p>
 
 <div class="realisateur-card-list">
-    <table>
-        <?php while ($realisateur = $request->fetch()): ?>
-            <tr>
-                <td>
-                    <div class="realisateur-card-person">
-                        <a href="index.php?action=infosRealisateur&id=<?= $realisateur["id_realisateur"] ?>">
-                            <span><?= $realisateur["prenom"]." ".$realisateur["nom"] ?></span>
-                            <span><?= $realisateur["birthdate"] ?></span>
-                            <span><?= $realisateur["sexe"] ?></span>
-                        </a>
-                    </div>
-                    <!-- IMAGE -->
-                    <?php 
-                    $imagePath = "public/images/imgRealisateurs/";
-                    $imageFilename = $realisateur["path_img_realisateur"];
-                    $imageUrl = $imagePath . $imageFilename;
-                    ?>
-                    <a href="index.php?action=infosRealisateur&id=<?= $realisateur["id_realisateur"] ?>">
-                        <img class="image-realisateur" src="<?= $imageUrl ?>" alt="photo du réalisateur <?= ucfirst($realisateur["nom"]) . " " . ucfirst($realisateur["prenom"]) ?>">
-                    </a>
-                </td>
-            </tr>
-        <?php endwhile; ?>
-    </table>
+    <?php while ($realisateur = $request->fetch()): ?>
+        <div class="realisateur-card">
+            <a href="index.php?action=infosRealisateur&id=<?= $realisateur["id_realisateur"] ?>">
+                <div class="realisateur-card-infos">
+                    <span><?= $realisateur["prenom"]." ".$realisateur["nom"] ?></span>
+                    <span><?= $realisateur["birthdate"] ?></span>
+                    <span><?= $realisateur["sexe"] ?></span>
+                </div>
+                <!-- IMAGE -->
+                <?php 
+                $imagePath = "public/images/imgRealisateurs/";
+                $imageFilename = $realisateur["path_img_realisateur"];
+                $imageUrl = $imagePath . $imageFilename;
+                ?>
+                <img class="image-realisateur" src="<?= $imageUrl ?>" alt="photo du réalisateur <?= ucfirst($realisateur["nom"]) . " " . ucfirst($realisateur["prenom"]) ?>">
+            </a>
+        </div>
+    <?php endwhile; ?>
 </div>
 
 <?php
