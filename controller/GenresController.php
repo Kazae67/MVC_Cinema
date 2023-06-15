b49250955975e329ecb3d344f0f3a55f56446425
Kazae67 <Kazae.zaeka@gmail.com>
2023-06-15 15:40:21 +0200

    ajout GenresController requête listGenres()

--- controller/GenresController.php
+++ controller/GenresController.php
@@ -5,4 +5,16 @@ namespace Controller;
 use Model\Connect;
 
 class GenresController {
+    public function listGenres()
+	{
+
+	$pdo = Connect::connectToDb();
+    
+	$request = $pdo->query("
+    SELECT genre_name, id_genre
+    FROM genre 
+    ");
+
+		require "view/listGenres.php";
+	}
 }
