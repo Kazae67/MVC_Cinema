5f78a1ace7154164f3c40489267fead0113fbae5
Kazae67 <Kazae.zaeka@gmail.com>
2023-06-15 11:45:23 +0200

    ajout des cards css pour les images roles

--- view/role/listRoles.php
+++ view/role/listRoles.php
@@ -1,31 +1,28 @@
 
 <?php ob_start(); ?>
 
-
 <!-- TOTAL RÔLES -->
 <?php $liste = "Les rôles disponible" . (($request->rowCount() > 1) ? "s" : ""); ?>
 <p class="row-count-list">Un total de <?= $request->rowCount() ?> rôle<?= ($request->rowCount() > 1) ? "s" : "" ?> disponible<?= ($request->rowCount() > 1) ? "s" : "" ?></p>
 
 <!-- LISTE DES RÔLES -->
 <div class="role-card-list">
-<?php foreach ($request->fetchAll() as $role): ?>
-    <a href="index.php?action=infosRole&id=<?= $role["id_role"] ?>">
-        <div class="role-card">
-            <div class="role-card-infos">
-                <span><?= "Nom : ".$role["role_name"] ?></span>
+    <?php foreach ($request->fetchAll() as $role): ?>
+        <a href="index.php?action=infosRole&id=<?= $role["id_role"] ?>">
+            <div class="role-card">
+                <div class="role-card-infos">
+                    <span><?= "Role : ".$role["role_name"] ?></span>
+                </div>
+                <!-- IMAGE -->
+                <?php
+                $imagePath = "public/images/imgRoles/";
+                $imageFileName = $role["path_img_role"];
+                $imageUrl = $imagePath . $imageFileName;
+                ?>
+                <img class="image-role" src="<?= $imageUrl ?>" alt="photo du role <?= ucfirst($role["role_name"]) ?>">
             </div>
-        </div>
-        <!-- IMAGE -->
-        <?php
-        $imagePath = "public/images/imgRoles/";
-        $imageFileName = $role["path_img_role"];
-        $imageUrl = $imagePath . $imageFileName;
-        var_dump($role);
-
-        ?>
-        <img class="image-role" src="<?= $imageUrl ?>" alt="photo du role <?= ucfirst($role["role_name"]) ?>">
-    </a>
-<?php endforeach; ?>
+        </a>
+    <?php endforeach; ?>
 </div>
 
 <?php
