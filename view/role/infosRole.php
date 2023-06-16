ae199f652c23d292fb43ad2a7c236b7ca0d99ab8
Kazae67 <Kazae.zaeka@gmail.com>
2023-06-16 16:59:53 +0200

    changement ordre d'affichage infosRole.php

--- view/role/infosRole.php
+++ view/role/infosRole.php
@@ -1,23 +1,22 @@
 <?php
 ob_start();
-
-
 ?>
+
 <div class="container">
     <div class="content">
         <div class="role-card-list">
             <div class="role-card-infos">
                 <div class="role-card-detail">
-                    <?php if ($role && isset($role["films"])): ?>
-                        <span><b>Films :</b> <?= $role["films"] ?></span>
-                    <?php endif; ?>
-                    <?php if ($role && isset($role["description"])): ?>
-                        <span><b>Description :</b> <?= $role["description"] ?></span>
-                    <?php endif; ?>
                     <?php if ($acteur && isset($acteur["prenom"]) && isset($acteur["nom"])): ?>
                         <span><b>Acteur :</b> <?= $acteur["prenom"] . " " . $acteur["nom"] ?></span>
                     <?php else: ?>
                         <span><b>Acteur :</b> Aucun acteur trouvé</span>
+                    <?php endif; ?>
+                    <?php if ($role && isset($role["description"])): ?>
+                        <span><b>Description :</b> <?= $role["description"] ?></span>
+                    <?php endif; ?>
+                    <?php if ($role && isset($role["films"])): ?>
+                        <span><b>Films :</b> <?= $role["films"] ?></span>
                     <?php endif; ?>
                     <?php if ($role && isset($role["titre_film"])): ?>
                         <span><b>Film :</b> <?= $role["titre_film"] ?></span>
@@ -27,6 +26,7 @@ ob_start();
         </div>
     </div>
 </div>
+
 <?php
 $content = ob_get_clean();
 $cssLink = '<link rel="stylesheet" href="public/css/role/infoRole.css">';
