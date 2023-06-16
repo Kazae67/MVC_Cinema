8730ab35472be26373dcde2f45b1d1d75a79811e
Kazae67 <Kazae.zaeka@gmail.com>
2023-06-16 10:38:41 +0200

    ajout du listCastings avec global empty pour ne pas afficher exemple : 'prenom' si tableau est vide

---
+++ view/casting/listCastings.php
@@ -0,0 +1,32 @@
+<?php
+
+ob_start();
+?>
+<?php $liste = "Les casting".($request->rowCount() > 1 ? "s" : "")." disponible".($request->rowCount() > 1 ? "s" : ""); ?>
+<p class="row-count-list"> Un total de <?= $request->rowCount() ?> casting<?= ($request->rowCount() > 1) ? "s" : "" ?> disponible<?= ($request->rowCount() > 1) ? "s" : "" ?></p>
+
+<div class="film-card-list">
+    <?php
+    foreach ($request->fetchAll() as $casting) { ?>
+        <div class="film-card-person">
+            <?php if (!empty($casting["prenom"])) { ?>
+                <span>Prénom : <a href="index.php?action=infosActeur&id=<?= $casting['id_acteur'] ?>"><?= $casting["prenom"] ?></a></span>
+            <?php } ?>
+            <?php if (!empty($casting["nom"])) { ?>
+                <span>Nom : <a href="index.php?action=infosActeur&id=<?= $casting['id_acteur'] ?>"><?= $casting["nom"] ?></a></span>
+            <?php } ?>
+            <?php if (!empty($casting["titre_film"])) { ?>
+                <span>Film : <a href="index.php?action=infosFilm&id=<?= $casting['id_film'] ?>"><?= $casting["titre_film"] ?></a></span>
+            <?php } ?>
+            <?php if (!empty($casting["role_name"])) { ?>
+                <span>Rôle : <a href="index.php?action=infosRole&id=<?= $casting['id_role'] ?>"><?= $casting["role_name"] ?></a></span>
+            <?php } ?>
+        </div>
+    <?php } ?>
+</div>
+
+<?php
+
+$content = ob_get_clean();
+
+require "view/template.php";
