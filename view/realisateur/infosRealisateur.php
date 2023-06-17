bffb5b632e6c423bad39a7a56c12c60cd337e947
Kazae67 <Kazae.zaeka@gmail.com>
2023-06-17 18:39:23 +0200

    amélioration structure infosRealisateur.php + infosRealisateur.css

--- view/realisateur/infosRealisateur.php
+++ view/realisateur/infosRealisateur.php
@@ -1,52 +1,52 @@
 <?php
 ob_start();
+$imagePath = 'public/images/imgFilms/';
 ?>
-<div class="film-acteur-card-list">
-    <div class="film-acteur-card-detail">
-        <?php if (isset($request_realisateur_infos)) {
-            $realisateur_infos = $request_realisateur_infos->fetch(); ?>
-            <h2><?= $realisateur_infos["prenom"] . " " . $realisateur_infos["nom"] ?></h2>
 
-            <p class="row-count-list">
-                Ce réalisateur a réalisé un total de : <?= $request_realisateur_list_films->rowCount() ?> film<?= ($request_realisateur_list_films->rowCount() > 1) ? "s" : "" ?>
-            </p>
-            <span>Né le : <?= $realisateur_infos["birthdate"] ?></span>
+<div class="container">
+    <div class="content">
+        <div class="realisateur-card-detail">
+            <?php if (isset($request_realisateur_infos)) {
+                $realisateur_infos = $request_realisateur_infos->fetch(); ?>
+                <h2><?= $realisateur_infos["prenom"] . " " . $realisateur_infos["nom"] ?></h2>
 
-            <?php if (!empty($realisateur_infos["biographie"])) { ?>
-                <h2>Biographie</h2>
-                <p class="biographie"><?= $realisateur_infos["biographie"] ?></p>
+                <p class="realisateur-info">Ce réalisateur a réalisé un total de : <?= $request_realisateur_list_films->rowCount() ?> film<?= ($request_realisateur_list_films->rowCount() > 1) ? "s" : "" ?></p>
+                <p>Né le : <?= $realisateur_infos["birthdate"] ?></p>
+
+                <?php if (isset($realisateur_infos["biographie"])): ?>
+                    <span class="realisateur-info"><b>Biographie :</b> <?= $realisateur_infos["biographie"] ?></span>
+                <?php endif; ?>
+
+                <span>
+                    <b>Film<?= ($request_realisateur_list_films->rowCount() > 1) ? "s" : "" ?> réalisé<?= ($request_realisateur_list_films->rowCount() > 1) ? "s" : "" ?> :</b>
+                </span>
             <?php } ?>
 
-            <span>
-                Film<?= ($request_realisateur_list_films->rowCount() > 1) ? "s" : "" ?> réalisé<?= ($request_realisateur_list_films->rowCount() > 1) ? "s" : "" ?> :
-            </span>
-        <?php } ?>
-
-        <?php if ($request_realisateur_list_films->rowCount() > 0) { ?>
-            <div class="film-acteur-card-horizontal">
-                <?php foreach ($request_realisateur_list_films->fetchAll() as $realisateur_film) { ?>
-                    <div class="film-container">
-                        <a href="index.php?action=infosFilm&id=<?= $realisateur_film['id_film'] ?>">
-                            <div class="film-acteur-card">
-                                <!-- IMAGE -->
-                                <?php 
-                                $imagePath = "public/images/imgFilms/";
-                                $imageFilename = $realisateur_film["path_img_film"];
-                                $imageUrl = $imagePath . $imageFilename;
-                                ?>
-                                <img class="image-acteur-film" src="<?= $imageUrl ?>" alt="affiche du film <?= $realisateur_film['titre_film'] ?>">
-                                <div class="film-info">
-                                    <span class="film-title"><?= $realisateur_film['titre_film'] ?></span>
-                                    <span class="film-date">Date : <?= $realisateur_film['date_sortie'] ?></span>
+            <?php if ($request_realisateur_list_films->rowCount() > 0) { ?>
+                <div class="realisateur-card-horizontal">
+                    <?php foreach ($request_realisateur_list_films->fetchAll() as $realisateur_film) { ?>
+                        <div class="realisateur-container">
+                            <a href="index.php?action=infosFilm&id=<?= $realisateur_film['id_film'] ?>">
+                                <div class="realisateur-card">
+                                    <!-- IMAGE -->
+                                    <img class="realisateur-image" src="<?= $imagePath . $realisateur_film['path_img_film'] ?>" alt="affiche du film <?= $realisateur_film['titre_film'] ?>">
+                                    <div class="realisateur-info">
+                                        <h3 class="realisateur-title"><a class="realisateur-link" href="index.php?action=infosFilm&id=<?= $realisateur_film['id_film'] ?>"><?= $realisateur_film['titre_film'] ?></a>
+                                            <?php if(isset($realisateur_film['role_name'])): ?>
+                                                <span class="realisateur-role"><a class="realisateur-link" href="index.php?action=infosRole&id=<?= $realisateur_film['id_role'] ?>"><?= $realisateur_film['role_name'] ?></a></span>
+                                            <?php endif; ?>
+                                            <span class="realisateur-date"><?= $realisateur_film['date_sortie'] ?></span>
+                                        </h3>
+                                    </div>
                                 </div>
-                            </div>
-                        </a>
-                    </div>
-                <?php } ?>
-            </div>
-        <?php } else { ?>
-            <span class="error">Ce réalisateur n'a réalisé aucun film.</span>
-        <?php } ?>
+                            </a>
+                        </div>
+                    <?php } ?>
+                </div>
+            <?php } else { ?>
+                <span class="error">Ce réalisateur n'a réalisé aucun film.</span>
+            <?php } ?>
+        </div>
     </div>
 </div>
 
