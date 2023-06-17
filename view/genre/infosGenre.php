97dafcb9af400fb30f0640d34c3a1cf38cfbb895
Kazae67 <Kazae.zaeka@gmail.com>
2023-06-17 22:39:30 +0200

    modification amélioré infosGenre.css + @media

--- view/genre/infosGenre.php
+++ view/genre/infosGenre.php
@@ -1,52 +1,55 @@
-<?php ob_start(); ?>
+<?php 
+ob_start(); 
+$imagePath = "public/images/imgFilms/";
+?>
 
-<?php if (isset($request_genre_infos)) {
-    $genre_infos = $request_genre_infos->fetch();
-} ?>
 
-<?php if ($request_genre_list_films->rowCount() > 0) { ?>
-    <?php
-    $total_films_genre = $request_genre_list_films->rowCount();
+<div class="container">
+    <div class="content">
+        <?php if (isset($request_genre_infos)) {
+            $genre_infos = $request_genre_infos->fetch();
+        } ?>
 
-    echo "<p class='row-count-list'>Dans ce genre, il y a un total de : $total_films_genre film" . ($total_films_genre > 1 ? "s" : "") . "</p>";
-    ?>
+        <?php if ($request_genre_list_films->rowCount() > 0) { ?>
+            <?php
+            $total_films_genre = $request_genre_list_films->rowCount();
+            echo "<p class='row-count-list'>Dans ce genre, il y a un total de : $total_films_genre film" . ($total_films_genre > 1 ? "s" : "") . "</p>";
+            ?>
 
-    <div class="film-genre-card-list">
-        <div class="film-genre-card-horizontal">
-            <?php foreach ($request_genre_list_films->fetchAll() as $genre_list_films) { ?>
-                <div class="film-genre-container">
-                    <a href="index.php?action=infosFilm&id=<?= $genre_list_films['id_film'] ?>">
-                        <div class="film-genre-card">
-                            <?php
-                            $imagePath = "public/images/imgFilms/";
-                            $imageFilename = $genre_list_films["path_img_film"];
-                            $imageUrl = $imagePath . $imageFilename;
-                            ?>
-                            <img class="image-genre-film" src="<?= $imageUrl ?>" alt="affiche du film <?= $genre_list_films['titre_film'] ?>">
-                            <div class="film-genre-info">
-                                <h2 class="film-genre-title"><?= $genre_list_films['titre_film'] ?></h2>
-                                <p class="film-genre-date">Date : <?= $genre_list_films['date_sortie'] ?></p>
-                                <?php
-                                $minutes = $genre_list_films["duree"];
-                                $heures = floor($minutes / 60);
-                                $minutes_restantes = $minutes % 60;
-                                ?>
-                                <p class="film-genre-duree">Durée: <?= $heures . "h" . $minutes_restantes . " mins" ?></p>
-                            </div>
+            <div class="film-genre-card-list">
+                <div class="film-genre-card-horizontal">
+                    <?php foreach ($request_genre_list_films->fetchAll() as $genre_list_films) { ?>
+                        <div class="film-genre-container">
+                            <a href="index.php?action=infosFilm&id=<?= $genre_list_films['id_film'] ?>">
+                                <div class="film-genre-card">
+                                    <img class="film-genre-image" src="<?= $imagePath . $genre_list_films['path_img_film'] ?>" alt="affiche du film <?= $genre_list_films['titre_film'] ?>">
+                                    <div class="film-genre-info">
+                                        <h2 class="film-genre-title"><?= $genre_list_films['titre_film'] ?></h2>
+                                        <p class="film-genre-date">Date : <?= $genre_list_films['date_sortie'] ?></p>
+                                        <?php
+                                        $minutes = $genre_list_films['duree'];
+                                        $heures = floor($minutes / 60);
+                                        $minutes_restantes = $minutes % 60;
+                                        ?>
+                                        <p class="film-genre-duree">Durée: <?= $heures . "h" . $minutes_restantes . " mins" ?></p>
+                                    </div>
+                                </div>
+                            </a>
                         </div>
-                    </a>
+                    <?php } ?>
                 </div>
-            <?php } ?>
-        </div>
-    </div>
+            </div>
 
-<?php } else { ?>
-    <div class="film-genre-card-list">
-        <div class="film-genre-card-detail">
-            <span class="error">Aucun film dans ce genre</span>
-        </div>
+        <?php } else { ?>
+            <div class="film-genre-card-list">
+                <div class="film-genre-card-detail">
+                    <span class="error">Aucun film dans ce genre</span>
+                </div>
+            </div>
+        <?php } ?>
+
     </div>
-<?php } ?>
+</div>
 
 <?php $content = ob_get_clean(); ?>
 <?php $cssLink = '<link rel="stylesheet" href="public/css/genre/infosGenre.css">'; ?>
