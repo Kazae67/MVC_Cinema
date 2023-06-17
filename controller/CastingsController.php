6cfb9a12e7deca573a6f5bff0bb9026acf2d502c
Kazae67 <Kazae.zaeka@gmail.com>
2023-06-17 23:25:55 +0200

    amélioration listCastings.php en ajoutant les images

--- controller/CastingsController.php
+++ controller/CastingsController.php
@@ -11,7 +11,7 @@ class CastingsController {
 		$pdo = Connect::seConnecter();
 		$request = $pdo->query("
 		
-			SELECT prenom, nom, role_name, titre_film, id_film, id_role, id_acteur
+			SELECT prenom, nom, role_name, titre_film, id_film, id_role, id_acteur, path_img_film, path_img_acteur, path_img_role
 			FROM film
 			INNER JOIN casting ON casting.film_id = film.id_film
 			INNER JOIN role ON casting.role_id = role.id_role
--- view/casting/listCastings.php
+++ view/casting/listCastings.php
@@ -1,26 +1,45 @@
 <?php
-
 ob_start();
+$imagePathFilm = 'public/images/imgFilms/';
+$imagePathActeur = 'public/images/imgActeurs/';
+$imagePathRole = 'public/images/imgRoles/';
 ?>
+
 <?php $liste = "Nombre de Casting".($request->rowCount() > 1 ? "s" : "")." disponible".($request->rowCount() > 1 ? "s" : ""); ?>
 <p class="row-count-list"> Un total de <?= $request->rowCount() ?> casting<?= ($request->rowCount() > 1) ? "s" : "" ?> disponible<?= ($request->rowCount() > 1) ? "s" : "" ?></p>
 
 <div class="film-card-list">
     <?php
     foreach ($request->fetchAll() as $casting) { ?>
-        <div class="film-card-person">
-            <?php if (!empty($casting["prenom"])) { ?>
-                <span>Prénom : <a href="index.php?action=infosActeur&id=<?= $casting['id_acteur'] ?>"><?= $casting["prenom"] ?></a></span>
-            <?php } ?>
-            <?php if (!empty($casting["nom"])) { ?>
-                <span>Nom : <a href="index.php?action=infosActeur&id=<?= $casting['id_acteur'] ?>"><?= $casting["nom"] ?></a></span>
-            <?php } ?>
-            <?php if (!empty($casting["titre_film"])) { ?>
-                <span>Film : <a href="index.php?action=infosFilm&id=<?= $casting['id_film'] ?>"><?= $casting["titre_film"] ?></a></span>
-            <?php } ?>
-            <?php if (!empty($casting["role_name"])) { ?>
-                <span>Rôle : <a href="index.php?action=infosRole&id=<?= $casting['id_role'] ?>"><?= $casting["role_name"] ?></a></span>
-            <?php } ?>
+        <div class="casting-container">
+            <div class="casting-images-container">
+                <?php if (!empty($casting["prenom"])) { ?>
+                    <div class="image-container">
+                        <img class="casting-image" src="<?= $imagePathActeur . $casting['path_img_acteur'] ?>" alt="affiche de l'acteur <?= $casting['prenom'] ?>">
+                    </div>
+                <?php } ?>
+                <?php if (!empty($casting["titre_film"])) { ?>
+                    <div class="image-container">
+                        <img class="casting-image" src="<?= $imagePathFilm . $casting['path_img_film'] ?>" alt="affiche du film <?= $casting['titre_film'] ?>">
+                    </div>
+                <?php } ?>
+                <?php if (!empty($casting["role_name"])) { ?>
+                    <div class="image-container">
+                        <img class="casting-image" src="<?= $imagePathRole . $casting['path_img_role'] ?>" alt="affiche du rôle <?= $casting['role_name'] ?>">
+                    </div>
+                <?php } ?>
+            </div>
+            <div class="casting-info">
+                <?php if (!empty($casting["prenom"])) { ?>
+                    <span>Prénom : <a href="index.php?action=infosActeur&id=<?= $casting['id_acteur'] ?>"><?= $casting["prenom"] ?></a></span>
+                <?php } ?>
+                <?php if (!empty($casting["role_name"])) { ?>
+                    <span>Rôle : <a href="index.php?action=infosRole&id=<?= $casting['id_role'] ?>"><?= $casting["role_name"] ?></a></span>
+                <?php } ?>
+                <?php if (!empty($casting["titre_film"])) { ?>
+                    <span>Film : <a href="index.php?action=infosFilm&id=<?= $casting['id_film'] ?>"><?= $casting["titre_film"] ?></a></span>
+                <?php } ?>
+            </div>
         </div>
     <?php } ?>
 </div>
