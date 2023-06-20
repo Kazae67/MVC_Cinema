1d64df722b166c0295b1a16408779e1fe1ecbbee
Kazae67 <Kazae.zaeka@gmail.com>
2023-06-20 23:21:58 +0200

    ajout commentaires index.php

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 
+// Importation des différents contrôleurs
 use Controller\FilmsController;
 use Controller\ActeursController;
 use Controller\RolesController;
@@ -9,10 +10,13 @@ use Controller\CastingsController;
 use Controller\FormulairesController;
 
 /* AUTO LOAD */
+// Enregistrement de la fonction d'auto-chargement de classe
 spl_autoload_register(function ($class_name) {
     include $class_name . '.php';
 });
 
+/* CONTROLLERS */
+// Instanciation des différents contrôleurs
 $FilmsController = new FilmsController();
 $ActeursController = new ActeursController();
 $RolesController = new RolesController();
@@ -22,11 +26,13 @@ $CastingsController = new CastingsController();
 $FormulairesController = new FormulairesController();
 
 /* FILTER */
+// Vérification et filtrage du paramètre "id"
 if (isset($_GET["id"])) {
     $id = filter_var($_GET["id"], FILTER_VALIDATE_INT, FILTER_NULL_ON_FAILURE);
 }
 
 /* SWITCH */
+// Vérification et exécution des actions basées sur le paramètre "action"
 if (isset($_GET["action"])) {
     switch ($_GET["action"]) {
         /* Liste des FILMS */
@@ -45,7 +51,7 @@ if (isset($_GET["action"])) {
         case "infosActeur":
             $ActeursController->infosActeur($id);
             break;
-        /* Liste des ROLE */
+        /* Liste des ROLES */
         case "listRoles":
             $RolesController->listRoles();
             break;
@@ -69,35 +75,37 @@ if (isset($_GET["action"])) {
         case "infosGenre":
             $GenresController->infosGenre($id);
             break;
-        /* Liste CASTINGS */
+        /* Liste des CASTINGS */
         case "listCastings":
             $CastingsController->listCastings();
             break;
-        /* Ajouter ACTEUR */
+        /* Ajouter un ACTEUR */
         case "ajouterActeur":
             $FormulairesController->ajouterActeur();
             break;
-        /* Ajouter ROLE */
+        /* Ajouter un ROLE */
         case "ajouterRole":
             $FormulairesController->ajouterRole();
             break;
-        /* Ajouter GENRE */
+        /* Ajouter un GENRE */
         case "ajouterGenre":
             $FormulairesController->ajouterGenre();
             break;
-        /* Ajouter REALISATEUR */
+        /* Ajouter un REALISATEUR */
         case "ajouterRealisateur":
             $FormulairesController->ajouterRealisateur();
             break;
-        /* Ajouter FILM */
+        /* Ajouter un FILM */
         case "ajouterFilm":
             $FormulairesController->ajouterFilm();
             break;
+        /* Ajouter un CASTING */
         case "ajouterCasting":
             $FormulairesController->ajouterCasting();
             break;
     }
 } else {
+    // Si aucune action n'est spécifiée, affiche la liste des films par défaut
     $FilmsController->listFilms();
 }
 
