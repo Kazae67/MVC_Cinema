5a7cc4003e73f51bfb56079694b0de52a31c0efc
Kazae67 <Kazae.zaeka@gmail.com>
2023-06-20 23:31:23 +0200

    commentaires améliorés template.php

--- view/template.php
+++ view/template.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://kit.fontawesome.com/a45e9c27c8.js" crossorigin="anonymous"></script>
-    <!-- LOAD UNIQUE -->
+    <!-- CSSLINK (génère dynamiquement le lien vers une page de style spécifique) -->
     <?= $cssLink ?>
     <!-- CONDITION ÉTOILES -->
     <link rel="stylesheet" href="public/css/film/etoiles.css">
@@ -22,6 +22,7 @@
     <!-- NAVBAR -->
     <main>
         <nav>
+            <!-- LOGO  -->
             <h2 class="logo"><a href="index.php?action=listFilms">Cinema</a></h2>
             <ul class="menu">
                 <li><a href="index.php?action=listFilms">Films</a></li>
@@ -30,8 +31,10 @@
                 <li><a href="index.php?action=listRealisateurs">Réalisateurs</a></li>
                 <li><a href="index.php?action=listGenres">Genres</a></li>
                 <li><a href="index.php?action=listCastings">Castings</a></li>
+                <!-- Dropdown -->
                 <li class="dropdown">
                     <a href="#">Formulaires</a>
+                    <!-- (menu déroulant) -->
                     <ul class="dropdown-menu">
                         <li><a href="index.php?action=ajouterFilm">Ajouter un film</a></li>
                         <li><a href="index.php?action=ajouterActeur">Ajouter un acteur</a></li>
@@ -44,9 +47,11 @@
             </ul>
         </nav>
         <div>
+            <!-- Vérifie si la variable $liste existe. Si c'est le cas, un titre de liste est affiché -->
             <?php if (isset($liste)): ?>
                 <h2 class="list-cards"><?= $liste ?></h2>
             <?php endif; ?>
+            <!-- Vérifie si la variable $content est définie. Si c'est le cas, le contenu dynamique est inséré -->
             <?php if (isset($content)): ?>
                 <?= $content ?>
             <?php endif; ?>
