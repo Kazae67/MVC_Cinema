b7a89cbfab80e5573a535b9100b22bb8a786b320
Kazae67 <Kazae.zaeka@gmail.com>
2023-06-20 18:07:35 +0200

    sauvegarde

--- view/formulaires/ajouterCasting.php
+++ view/formulaires/ajouterCasting.php
@@ -2,6 +2,10 @@
 ob_start();
 ?>
 
+<form action="index.php?action=ajouterCasting" method="post">
+
+
+</form>
 
 <?php
 $cssLink = '<link rel="stylesheet" href="public/css/formulaires/formulaires.css">';
