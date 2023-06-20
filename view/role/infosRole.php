9c7d6b2f3fcaba569fb19d6bd0140eb6247ba07a
Kazae67 <Kazae.zaeka@gmail.com>
2023-06-20 23:52:21 +0200

    commentaires améliorés list & infos role(s)

--- view/role/infosRole.php
+++ view/role/infosRole.php
@@ -10,26 +10,32 @@ $imagePath = 'public/images/imgFilms/';
                 <div class="role-card-detail">
                     
                     <?php if ($acteur && isset($acteur["prenom"]) && isset($acteur["nom"])): ?>
+                        <!-- Vérifie si l'acteur existe et si les valeurs 'prenom' et 'nom' sont définies -->
                         <span class="film-info"><b>Acteur :</b> 
                             <a href="index.php?action=infosActeur&id=<?= $acteur['id_acteur'] ?>">
                                 <?= $acteur["prenom"] . " " . $acteur["nom"] ?>
                             </a>
                         </span>
                     <?php else: ?>
+                        <!-- Si l'acteur n'est pas trouvé, affiche un message d'erreur -->
                         <span class="film-info"><b>Acteur :</b> Aucun acteur trouvé</span>
                     <?php endif; ?>
 
                     <?php if ($role && isset($role["description"])): ?>
+                        <!-- Vérifie si le rôle existe et si la valeur 'description' est définie -->
                         <span class="film-info"><b>Description :</b> <?= $role["description"] ?></span>
                     <?php endif; ?>
 
                     <?php if ($role && isset($role["films"])): ?>
+                        <!-- Vérifie si le rôle existe et si la valeur 'films' est définie -->
                         <span><b>Films :</b></span>
                         <div class="film-list">
                             <?php foreach ($films as $film): ?>
+                                <!-- Parcourt la liste des films -->
                                 <div class="film-item">
                                     <div class="film-container">
                                         <a href="index.php?action=infosFilm&id=<?= $film['id_film'] ?>">
+                                            <!-- Affiche l'image du film avec le lien vers les informations du film -->
                                             <img src="<?= $imagePath . $film['path_img_film'] ?>" alt="<?= $film['titre_film'] ?>" class="film-image">
                                         </a>
                                         <h3><span class="film-title"><?= $film['titre_film'] ?></span></h3>
@@ -40,6 +46,7 @@ $imagePath = 'public/images/imgFilms/';
                     <?php endif; ?>
 
                     <?php if ($role && isset($role["titre_film"])): ?>
+                        <!-- Vérifie si le rôle existe et si la valeur 'titre_film' est définie -->
                         <span class="film-info"><b>Film :</b> <?= $role["titre_film"] ?></span>
                     <?php endif; ?>
                 </div>
--- view/role/listRoles.php
+++ view/role/listRoles.php
@@ -1,19 +1,26 @@
-
+<!-- temporisation de sortie -->
 <?php ob_start(); ?>
 
-
+<!-- La variable $liste est définie avec un message indiquant le nombre de rôles disponibles. Si le nombre de rôles est supérieur à 1, le texte est au pluriel. -->
 <?php $liste = "Nombre de Rôle".($request->rowCount() > 1 ? "s" : "")." disponible".($request->rowCount() > 1 ? "s" : ""); ?>
+
+<!-- Afficher le nombre total de rôles disponibles en utilisant la fonction rowCount() de l'objet $request. -->
 <p class="row-count-list"> Un total de <?= $request->rowCount() ?> rôle<?= ($request->rowCount() > 1) ? "s" : "" ?> disponible<?= ($request->rowCount() > 1) ? "s" : "" ?></p>
 
 <!-- LISTE DES RÔLES -->
 <div class="role-card-list">
+    <!-- Boucle foreach pour itérer sur chaque rôle récupéré à partir de l'objet $request->fetchAll(). 
+    Chaque rôle est affiché sous forme de card avec un lien vers les informations détaillées du rôle. -->
     <?php foreach ($request->fetchAll() as $role): ?>
         <a href="index.php?action=infosRole&id=<?= $role["id_role"] ?>">
             <div class="role-card">
                 <div class="role-card-infos">
+                    <!-- À l'intérieur de chaque card de rôle, le nom du rôle affiché -->
                     <span><?= "Role : ".$role["role_name"] ?></span>
                 </div>
-                <!-- IMAGE -->
+                <!-- IMAGE --> 
+                <!-- L'image associée au rôle est affichée en utilisant les informations du chemin d'accès et du nom de fichier d'image stockées dans les variables. 
+                L'URL de l'image est générée en concaténant le chemin d'accès et le nom de fichier. -->
                 <?php
                 $imagePath = "public/images/imgRoles/";
                 $imageFileName = $role["path_img_role"];
