59739d5818d1244df083fc8ea37a5d11ddd8d1a6
Kazae67 <Kazae.zaeka@gmail.com>
2023-06-21 23:51:34 +0200

    commentaires infosRealisateur et infosRole

--- view/realisateur/infosRealisateur.php
+++ view/realisateur/infosRealisateur.php
@@ -6,35 +6,55 @@ $imagePath = 'public/images/imgFilms/';
 <div class="container">
     <div class="content">
         <div class="realisateur-card-detail">
-            <?php if (isset($request_realisateur_infos)) {
-                $realisateur_infos = $request_realisateur_infos->fetch(); ?>
+            <?php
+
+            /* PRENOM | NOM */
+            // Vérifier si les informations du réalisateur sont définies
+            if (isset($request_realisateur_infos)) {
+                $realisateur_infos = $request_realisateur_infos->fetch();
+                ?>
                 <h2><?= $realisateur_infos["prenom"] . " " . $realisateur_infos["nom"] ?></h2>
 
+                <!-- Afficher le nombre de films réalisés -->
                 <p class="realisateur-info">Ce réalisateur a réalisé un total de : <?= $request_realisateur_list_films->rowCount() ?> film<?= ($request_realisateur_list_films->rowCount() > 1) ? "s" : "" ?></p>
                 <p>Né le : <?= $realisateur_infos["birthdate"] ?></p>
 
-                <?php if (isset($realisateur_infos["biographie"])): ?>
+                
+                <?php
+                /* BIOGRAPHIE */
+                // Vérifier si la biographie du réalisateur est définie
+                if (isset($realisateur_infos["biographie"])) {
+                    ?>
                     <span class="realisateur-info"><b>Biographie :</b> <?= $realisateur_infos["biographie"] ?></span>
-                <?php endif; ?>
+                <?php } ?>
 
                 <span></span>
                     <b>Film<?= ($request_realisateur_list_films->rowCount() > 1) ? "s" : "" ?> réalisé<?= ($request_realisateur_list_films->rowCount() > 1) ? "s" : "" ?> :</b>
                 </span>
             <?php } ?>
 
-            <?php if ($request_realisateur_list_films->rowCount() > 0) { ?>
+            <?php
+            // Vérifier si des films sont disponibles
+            if ($request_realisateur_list_films->rowCount() > 0) {
+                ?>
                 <div class="realisateur-card-horizontal">
-                    <?php foreach ($request_realisateur_list_films->fetchAll() as $realisateur_film) { ?>
+                    <?php
+                    // Parcourir tous les films du réalisateur
+                    foreach ($request_realisateur_list_films->fetchAll() as $realisateur_film) {
+                        ?>
                         <div class="realisateur-container">
                             <a href="index.php?action=infosFilm&id=<?= $realisateur_film['id_film'] ?>">
                                 <div class="realisateur-card">
-                                    <!-- IMAGE -->
+                                    <!-- IMAGE FILM -->
                                     <img class="realisateur-image" src="<?= $imagePath . $realisateur_film['path_img_film'] ?>" alt="affiche du film <?= $realisateur_film['titre_film'] ?>">
                                     <div class="realisateur-info">
                                         <h3 class="realisateur-title"><a class="realisateur-link" href="index.php?action=infosFilm&id=<?= $realisateur_film['id_film'] ?>"><?= $realisateur_film['titre_film'] ?></a>
-                                            <?php if(isset($realisateur_film['role_name'])): ?>
+                                            <?php
+                                            // Vérifier si le nom du rôle est défini
+                                            if (isset($realisateur_film['role_name'])) {
+                                                ?>
                                                 <span class="realisateur-role"><a class="realisateur-link" href="index.php?action=infosRole&id=<?= $realisateur_film['id_role'] ?>"><?= $realisateur_film['role_name'] ?></a></span>
-                                            <?php endif; ?>
+                                            <?php } ?>
                                             <span class="realisateur-date"><?= $realisateur_film['date_sortie'] ?></span>
                                         </h3>
                                     </div>
@@ -43,6 +63,7 @@ $imagePath = 'public/images/imgFilms/';
                         </div>
                     <?php } ?>
                 </div>
+                <!-- ERREUR -->
             <?php } else { ?>
                 <span class="error">Ce réalisateur n'a réalisé aucun film.</span>
             <?php } ?>
--- view/role/infosRole.php
+++ view/role/infosRole.php
@@ -9,33 +9,36 @@ $imagePath = 'public/images/imgFilms/';
             <div class="role-card-infos">
                 <div class="role-card-detail">
                     
+                    <!-- PRENOM | NOM -->
+                    <!-- Vérifie si l'acteur existe et si les valeurs 'prenom' et 'nom' sont définies -->
                     <?php if ($acteur && isset($acteur["prenom"]) && isset($acteur["nom"])): ?>
-                        <!-- Vérifie si l'acteur existe et si les valeurs 'prenom' et 'nom' sont définies -->
                         <span class="film-info"><b>Acteur :</b> 
                             <a href="index.php?action=infosActeur&id=<?= $acteur['id_acteur'] ?>">
                                 <?= $acteur["prenom"] . " " . $acteur["nom"] ?>
                             </a>
                         </span>
-                    <?php else: ?>
                         <!-- Si l'acteur n'est pas trouvé, affiche un message d'erreur -->
+                    <?php else: ?>
                         <span class="film-info"><b>Acteur :</b> Aucun acteur trouvé</span>
                     <?php endif; ?>
 
+                    <!-- DESCRIPTION -->
+                    <!-- Vérifie si le rôle existe et si la valeur 'description' est définie -->
                     <?php if ($role && isset($role["description"])): ?>
-                        <!-- Vérifie si le rôle existe et si la valeur 'description' est définie -->
                         <span class="film-info"><b>Description :</b> <?= $role["description"] ?></span>
                     <?php endif; ?>
 
+                    <!-- FILMS -->
+                    <!-- Vérifie si le rôle existe et si la valeur 'films' est définie -->
                     <?php if ($role && isset($role["films"])): ?>
-                        <!-- Vérifie si le rôle existe et si la valeur 'films' est définie -->
                         <span><b>Films :</b></span>
                         <div class="film-list">
+                            <!-- Parcourt la liste des films -->
                             <?php foreach ($films as $film): ?>
-                                <!-- Parcourt la liste des films -->
                                 <div class="film-item">
                                     <div class="film-container">
+                                        <!-- Affiche l'image du film avec le lien vers les informations du film -->
                                         <a href="index.php?action=infosFilm&id=<?= $film['id_film'] ?>">
-                                            <!-- Affiche l'image du film avec le lien vers les informations du film -->
                                             <img src="<?= $imagePath . $film['path_img_film'] ?>" alt="<?= $film['titre_film'] ?>" class="film-image">
                                         </a>
                                         <h3><span class="film-title"><?= $film['titre_film'] ?></span></h3>
@@ -45,8 +48,8 @@ $imagePath = 'public/images/imgFilms/';
                         </div>
                     <?php endif; ?>
 
+                    <!-- Vérifie si le rôle existe et si la valeur 'titre_film' est définie -->
                     <?php if ($role && isset($role["titre_film"])): ?>
-                        <!-- Vérifie si le rôle existe et si la valeur 'titre_film' est définie -->
                         <span class="film-info"><b>Film :</b> <?= $role["titre_film"] ?></span>
                     <?php endif; ?>
                 </div>
